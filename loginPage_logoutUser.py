9bdbd060cd1dec4be6ec7b2bce9b340920470bc8
bmhcode <51011661+bmhcode@users.noreply.github.com>
2024-11-21 15:43:19 +0100

    Add files via upload
    My first commit

---
+++ loginPage_logoutUser.py
@@ -0,0 +1,25 @@
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+            
+    context = {}
+    return render(request, 'base/login_register.html', context)
+#-------------------------------------------------------
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
